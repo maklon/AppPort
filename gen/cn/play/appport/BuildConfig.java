952e3518008d625730b94a49c3dda912d7963a38
Maklon-Due <Maklon@Maklon-Due>
2013-11-06 09:28:24 +0800

    du

---
+++ gen/cn/play/appport/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.play.appport;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/cn/play/appport/ApplianceDetail.java
+++ src/cn/play/appport/ApplianceDetail.java
@@ -4,14 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class ApplianceDetail extends Activity {
-    Activity thisActivity;
+	Activity thisActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.layout_app_detail);
-	thisActivity = this;
-    }
-    
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_app_detail);
+		thisActivity = this;
+	}
+
 }
